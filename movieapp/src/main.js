b9658bd30583f3027ac573ee25e1b5a24f525f77
dodyvirgiawan <dody.virgiawan97@gmail.com>
2021-08-24 20:33:49 +0700

    integrate with algolia search api

--- movieapp/src/main.js
+++ movieapp/src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import InstantSearch from 'vue-instantsearch'
 import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false
+
+Vue.use(InstantSearch)
 
 new Vue({
   router,
