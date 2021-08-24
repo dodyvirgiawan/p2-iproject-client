c68c5284548b7a1b9d04153c9b809bc6665688b5
dodyvirgiawan <dody.virgiawan97@gmail.com>
2021-08-24 11:43:21 +0700

    add initialization of routing

--- movieapp/src/router/index.js
+++ movieapp/src/router/index.js
@@ -1,29 +1,71 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    { 
+        path: '/login', 
+        name: 'Login', 
+        component: () => import('../views/Login.vue')
+    },
+    { 
+        path: '/register', 
+        name: 'Register', 
+        component: () => import('../views/Register.vue')
+    },
+    { 
+        path: '/', 
+        component: () => import('../views/AppTemplate.vue'),
+        children: [
+            { 
+                path: '/', 
+                name: 'Home', 
+                component: () => import('../views/Homepage.vue')
+            },
+            { 
+                path: 'movies', 
+                name: 'SearchMovie', 
+                component: () => import('../views/SearchMovie.vue')
+            },
+            {
+                path: 'movies/addtoplaylist',
+                name: 'AddToPlaylist',
+                component: () => import('../views/AddToPlaylist.vue')
+            },
+            {
+                path: 'movies/createplaylist',
+                name: 'CreatePlaylist',
+                component: () => import('../views/CreatePlaylist.vue')
+            },
+            { 
+                path: 'playlist', 
+                name: 'Playlist', 
+                component: () => import('../views/UserPlaylist.vue')
+            },
+            {
+                path: 'playlist/edit/:id',
+                name: 'EditPlaylist',
+                component: () => import('../views/EditPlaylist.vue')
+            },
+            {
+                path: 'playlist/addmovie',
+                name: 'AddMovie',
+                component: () => import('../views/AddMovie.vue')
+            },
+            {
+                path: 'playlist/:id',
+                name: 'PlaylistDetail',
+                component: () => import('../views/PlaylistDetail.vue')
+            }
+        ]
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
