cc082dd9eb179b1c297f192fcd017055146ff90a
dodyvirgiawan <dody.virgiawan97@gmail.com>
2021-08-24 11:52:22 +0700

    commentcards

---
+++ movieapp/src/components/CommentCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex flex-row p-6 mc-3 mt-5 rounded-2xl">
+        <!-- Commenter's Name -->
+        <div class="container w-1/4 flex flex-col">
+            <h5>{{ comment.author.first_name }} {{ comment.author.last_name }}</h5>
+            <small>{{ countDays }}</small>
+        </div>
+
+        <!-- The Actual Comment -->
+        <div class="container w-3/4">
+            <p class="text-sm">
+                {{ comment.comment }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CommentCard',
+    props: ['comment'],
+    computed: {
+        countDays() {
+            const oneDay = 24 * 60 * 60 * 1000
+            const today = new Date()
+
+            const postedDay = new Date(this.comment.createdAt)
+            const output = Math.round(Math.abs((postedDay - today) / oneDay))
+
+            if(output === 0) return 'today'
+            else return `${output} day(s) ago`
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- movieapp/src/store/index.js
+++ movieapp/src/store/index.js
@@ -1,19 +1,202 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Toast from "vue-toastification"
+import router from '../router'
+import cineclubApi from '../apis/cineclubApi.js'
+import "vue-toastification/dist/index.css"
+
+const toastOptions = {
+    position: "top-center",
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+  }
+
+Vue.use(Toast, {
+    transition: "Vue-Toastification__fade",
+    maxToasts: 20,
+    newestOnTop: true,
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        isLoggedIn: ''
+        isLoggedIn: '',
+        loggedInUserInfo: {},
+        playlists: [],
+        loggedInUserPlaylists: [],
+        viewedPlaylist: {}
     },
     mutations: {
         SET_IS_LOGGED_IN(state, payload) {
-            state.isLoggedIn =  payload
+            state.isLoggedIn = payload
+        },
+
+        SET_LOGGED_IN_USER_INFO(state, payload) {
+            state.loggedInUserInfo = payload
+        },
+
+        SET_PLAYLISTS(state, payload) {
+            state.playlists = payload
+        },
+
+        SET_LOGGED_IN_USER_PLAYLISTS(state, payload) {
+            state.loggedInUserPlaylists = payload
+        },
+
+        SET_VIEWED_PLAYLIST(state, payload) {
+            state.viewedPlaylist = payload
         }
     },
     actions: {
+        async doRegister(context, payload) {
+            try {
+                await cineclubApi({
+                    method: 'POST',
+                    url: '/register',
+                    data: {
+                        first_name: payload.first_name,
+                        last_name: payload.last_name,
+                        email: payload.email,
+                        password: payload.password
+                    }
+                })
+                
+                Vue.$toast.success("Successfully registered! Please proceed to login!", toastOptions)
+                router.push('/login')
+            } catch (err) {
+                let errorMessages = ''
+
+                if(Array.isArray(err.response.data.message)) {
+                    err.response.data.message.forEach((el, idx) => {
+                        errorMessages += `${el}`
+                        errorMessages += idx === err.response.data.message.length - 1 ? '' : ',\n'
+                    })
+                } else {
+                    errorMessages = err.response.data.message
+                }
+
+                Vue.$toast.error(errorMessages, toastOptions);
+            }
+        },
+
+        async doLogin(context, payload) {
+            try {
+                const response = await cineclubApi({
+                    method: 'POST',
+                    url: '/login',
+                    data: {
+                        email: payload.email,
+                        password: payload.password
+                    }
+                })
+                
+                localStorage.setItem('access_token', response.data.access_token)
+                context.commit('SET_IS_LOGGED_IN', true)
+                context.dispatch('getLoggedInUserInfo')
+
+                Vue.$toast.success("Successfully logged in! Welcome to Cineclub!", toastOptions)
+                router.push('/')
+            } catch (err) {
+                Vue.$toast.error(err.response.data.message)
+            }
+        },
+
+        async getLoggedInUserInfo(context) {
+            try {
+                const response = await cineclubApi({
+                    method: 'GET',
+                    url: '/user',
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+
+                context.commit('SET_LOGGED_IN_USER_INFO', response.data)
+            } catch (err) {
+                Vue.$toast.error(err.response.data.message)
+            }
+        },
+
+        async fetchPlaylists(context) {
+            try {
+                const response = await cineclubApi({
+                    method: 'GET',
+                    url: '/playlists',
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+
+                context.commit('SET_PLAYLISTS', response.data)
+            } catch (err) {
+                Vue.$toast.error(err.response.data.message)
+            }
+        },
+
+        async fetchPlaylistById(context, payload) {
+            try {
+                const playlistId = payload
+
+                const response = await cineclubApi({
+                    method: 'GET',
+                    url: `/playlists/${playlistId}`,
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+
+                context.commit('SET_VIEWED_PLAYLIST', response.data)
+            } catch (err) {
+                Vue.$toast.error(err.response.data.message)
+            }
+        },
+
+        async fetchLoggedInUserPlaylist(context) {
+            try {
+                const response = await cineclubApi({
+                    method: 'GET',
+                    url: '/playlists/user',
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+                
+                context.commit('SET_LOGGED_IN_USER_PLAYLISTS', response.data)
+            } catch (err) {
+                Vue.$toast.error(err.response.data.message)
+            }
+        },
+
+        async addCommentToPlaylist(context, payload) {
+            try {
+                const response = await cineclubApi({
+                    method: 'POST',
+                    url: `/playlists/${payload.playlistId}/comments`,
+                    data: {
+                        comment: payload.comment
+                    },
+                    headers: {
+                        access_token: localStorage.getItem('access_token')
+                    }
+                })
+
+                Vue.$toast.success(response.data.message, toastOptions);
+                context.dispatch('fetchPlaylistById', payload.playlistId)
+            } catch (err) {
+                Vue.$toast.error(err.response.data.message, toastOptions);
+            }
+        }
     },
     modules: {
     }
-    })
+})
