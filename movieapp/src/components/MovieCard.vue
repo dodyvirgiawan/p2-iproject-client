13d0d5dddf5bb85fa16835df83b5622c146131eb
dodyvirgiawan <dody.virgiawan97@gmail.com>
2021-08-24 15:12:21 +0700

    search functionality through omdbApi

--- movieapp/src/components/MovieCard.vue
+++ movieapp/src/components/MovieCard.vue
@@ -11,22 +11,12 @@
                 <div class="container">
                     <small>{{ movie.runtime }} min | {{ movie.genre }}</small>
                 </div>
-                <div class="flex flex-row">
-                    <div class="container w-3/4">
-                        <div class="mt-6 text-xs">
-                            <p>Directed by: {{ movie.director }}</p>
-                        </div>
-                        <div class="mt-2 text-xs">
-                            <p>IMDB Rating: {{ movie.imdbRating }}</p>
-                        </div>
+                <div class="container">
+                    <div class="mt-6 text-xs">
+                        <p>Directed by: {{ movie.director }}</p>
                     </div>
-                    <div class="container w-1/4 m-auto">
-
-                        <!-- Kalau di search page, include button ini -->
-                        <!-- <button class="rounded-lg mc-button p-2 mt-6 block">
-                            <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                        </button> -->
-
+                    <div class="mt-2 text-xs">
+                        <p>IMDB Rating: {{ movie.imdbRating }}</p>
                     </div>
                 </div>
             </div>
--- movieapp/src/views/SearchMovie.vue
+++ movieapp/src/views/SearchMovie.vue
@@ -11,307 +11,47 @@
 
         <!-- Search Bar -->
         <div class="container mt-8 flex flex-row ml-5">
-            <input name="search" type="text" placeholder="search movies..." class="h-9 p-3 rounded-xl text-black">
-            <button class="rounded-lg p-2 mc-button my-auto block ml-5">Search</button>
+            <input v-model="searchTitle" name="search" type="text" placeholder="search movies..." class="h-9 p-3 rounded-xl text-black">
+            <button class="rounded-lg p-2 mc-button my-auto block ml-5" @click="searchMovie">Search</button>
         </div>
 
         <!-- Content -->
         <div class="container flex flex-row flex-wrap mt-4">
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- MOVIECARD -->
-            <div class="mc-3 mt-5 ml-4 rounded-xl">
-                <div class="container flex flex-col w-60">
-                    <div class="container">
-                        <img src="@/assets/samplemovie.jpg" class="object-cover rounded-t-xl">
-                    </div>
-                    <div class="container p-5">
-                        <div class="container">
-                            <h3 class="text-2xl">Blade Runner</h3>
-                        </div>
-                        <div class="container">
-                            <small>2013 | 89 min | Drama</small>
-                        </div>
-                        <div class="flex flex-row">
-                            <div class="container w-3/4">
-                                <div class="mt-6 text-xs">
-                                    <p>Directed by: John Doe</p>
-                                </div>
-                                <div class="mt-2 text-xs">
-                                    <p>IMDB Rating: 9.5</p>
-                                </div>
-                            </div>
-                            <div class="container w-1/4 m-auto">
-                                <button class="rounded-lg mc-button p-2 mt-6 block">
-                                    <img class="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAxUlEQVRIS92V0Q3CMAxE7yZghMImjEIng1EYpd2ADYwSKSgE4sQmVSX6W/tefHUvxMYPN9ZHFSAiZwBXAEfjIRYAM8l76NMAoXAyiqfyheSpBZBYQJpsFJG3Pm2C/QDlKXMbh0zgApRNFpGuCVqA9L62VWkZqhbtDtBsGGLRfwG0+HB/5N5MsgAeAA69wkXdSjKm8CuLvqxpiOubI1FXAJePuNb+XOcUsa06wS+ieW8OcHneui9ygMvzbsAoS0od03XoOcQTbKPOGc7x0aEAAAAASUVORK5CYII="/>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <MovieSearchCard
+                v-for="(movie, idx) in searchedMovies"
+                :key="movie.title + idx"
+                :movie="movie"
+            ></MovieSearchCard>
             
         </div>
     </div>
 </template>
 
 <script>
+import MovieSearchCard from '../components/MovieSearchCard.vue'
+
 export default {
-    name: 'SearchMovie'
+    name: 'SearchMovie',
+    components: {
+        MovieSearchCard
+    },
+    data: function() {
+        return {
+            searchTitle: ''
+        }
+    },
+    methods: {
+        searchMovie() {
+            this.$store.dispatch('searchMovieByTitle', this.searchTitle)
+        }
+    },
+    computed: {
+        searchedMovies() {
+            return this.$store.state.searchedMovies
+        }
+    }
 }
 </script>
 
